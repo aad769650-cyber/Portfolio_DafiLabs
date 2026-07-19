3f6592640bdc0f18d494fce99d368a3d7c2c351c
Mahar Abdul Qudoos <aad769650@gmail.com>
2026-07-19 16:00:44 +0500

    some glitches improved

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         
-\
+
      <Providers>
           {children}
           <Toaster richColors position="top-center" />
